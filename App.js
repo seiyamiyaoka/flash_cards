46eef7a575e63847b75dbe35b0fc9d2ec8b8f435
seiyamiyaoka <seiyamiyaoka@gmail.com>
2017-11-23 12:23:57 +0900

    create routes

--- App.js
+++ App.js
@@ -3,45 +3,8 @@ import { StyleSheet, Text, View, AsyncStorage } from 'react-native';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
-import History from './components/History'
-import NewDeck from './components/NewDeck'
-import AddCard from './components/AddCard'
 import { setLocalNotification } from './utils/helper'
-import DetailQuestion from './components/DetailQuestion'
-import QuestionForm from './components/QuestionForm'
-import { TabNavigator, StackNavigator } from 'react-navigation'
-
-
-const Tabs = TabNavigator({
-  Questions: {
-    screen: History,
-    tabBarLabel: 'Question',
-    tabBarIcon: ({tintColor}) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
-  },
-  NewDeck: {
-    screen: NewDeck,
-    tabBarLabel: 'NewDeck',
-    tabBarIcon: ({tintColor}) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
-  },
-  // tabBarOptions: {
-  //   activeTintColor:
-  // }
-})
-
-const MainNavigator = StackNavigator({
-  Home: {
-    screen: Tabs,
-  },
-  DetailQuestion: {
-    screen: DetailQuestion
-  },
-  AddCard: {
-    screen: AddCard
-  },
-  QuestionForm: {
-    screen: QuestionForm
-  }
-})
+import { MainNavigator } from './routes'
 
 export default class App extends React.Component {
   componentDidMount() {
@@ -57,8 +20,6 @@ export default class App extends React.Component {
     );
   }
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
---
+++ routes.js
@@ -0,0 +1,34 @@
+import History from './components/History'
+import NewDeck from './components/NewDeck'
+import AddCard from './components/AddCard'
+import DetailQuestion from './components/DetailQuestion'
+import QuestionForm from './components/QuestionForm'
+import { TabNavigator, StackNavigator } from 'react-navigation'
+
+const Tabs = TabNavigator({
+  Questions: {
+    screen: History,
+    tabBarLabel: 'Question',
+    tabBarIcon: ({tintColor}) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+  },
+  NewDeck: {
+    screen: NewDeck,
+    tabBarLabel: 'NewDeck',
+    tabBarIcon: ({tintColor}) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+  },
+})
+
+export const MainNavigator = StackNavigator({
+  Home: {
+    screen: Tabs,
+  },
+  DetailQuestion: {
+    screen: DetailQuestion
+  },
+  AddCard: {
+    screen: AddCard
+  },
+  QuestionForm: {
+    screen: QuestionForm
+  }
+})
